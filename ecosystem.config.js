91b63c94d037315dcfe39653939bef35c14605cb
Thanabodee Nilthongkum <thanaboody45@gmail.com>
2020-06-13 02:33:10 +0700

    setting ecosystem.config.js PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [{
+    name: 'Nuxt', // App name that shows in `pm2 ls`, the name you used to create your nuxt.js application
+    exec_mode: 'cluster', // enables clustering
+    instances: '2', // or an integer
+    cwd: './current', // only if using a subdirectory
+    "script": "npm",
+    "args": "start"
+  }]
+};
