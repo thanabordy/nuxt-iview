06a9371bf476f27e7b96d36b2607d4e48facd77e
Thanabodee Nilthongkum <thanaboody45@gmail.com>
2020-06-13 03:03:41 +0700

    tag a

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  apps: [{
-    name: 'Nuxt', // App name that shows in `pm2 ls`, the name you used to create your nuxt.js application
-    exec_mode: 'cluster', // enables clustering
-    instances: '2', // or an integer
-    cwd: './current', // only if using a subdirectory
-    "script": "npm",
-    "args": "start"
-  }]
-};
+  apps : [
+    {
+      name: "nuxt-dev",
+      script: "npm",
+      args: "run dev"
+    },
+    {
+      name: "nuxt-prod",
+      script: "npm",
+      args: "run start"
+    }
+  ]
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <Button to="/manga/search">Go To Search Page</Button>
+    <a href="./manga/search">
+      <v-btn color="info">Go To Search Page</v-btn>
+    </a>
+
+    <!-- <Button to="./manga/search">Go To Search Page</Button> -->
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style lang="css" scoped>
-
 </style>
